5e588993665c1787731bc442d383758ae10d8184
Oleksandra Landau <landausa23@gmail.com>
2023-03-29 14:28:04 +0300

    new commits

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/mypage/me")
+def mypage():
+    return render_template("1st_page.html")
+
+@app.route("/mypage/contact")
+def mypage2():
+    return render_template("2nd_page.html")
+
+
+@app.route('/message', methods=['POST'])
+def message():
+    print(request.form)
+    return redirect("/message")
+
+if __name__ == '__main__':
+    app.run(debug=True)
